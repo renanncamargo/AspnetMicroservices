84298b68ef44621b54ec57280520f551d2075a01
Renann Camargo Alves <renanncamargo@gmail.com>
2022-01-31 21:25:34 -0300

    Basket microservices developed

---
+++ src/Services/Basket/Basket.API/Entities/ShoppingCart.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace Basket.API.Entities
+{
+    public class ShoppingCart
+    {
+        public string Username { get; set; }
+        public List<ShoppingCartItem> Items { get; set; } = new List<ShoppingCartItem>();
+
+        public ShoppingCart(string username)
+        {
+            Username = username;
+        }
+
+        public decimal TotalPrice
+        {
+            get
+            {
+                decimal totalprice = 0;
+                foreach (var item in Items)
+                {
+                    totalprice += item.Price * item.Quantity;
+                }
+                return totalprice;
+            }
+        }
+    }
+}
